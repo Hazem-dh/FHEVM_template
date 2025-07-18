a73c57fb8e04a8d839b862e6748d8ef3865dbbba
hazem-dh <hazemdhaou.it@gmail.com>
2025-07-18 13:14:05 +0200

    remove blocker if PK not set up in .env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,6 +20,7 @@ const MNEMONIC = process.env.MNEMONIC || "test test test test test test test tes
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const sepoliaAccounts = PRIVATE_KEY ? [PRIVATE_KEY] : { mnemonic: MNEMONIC, path: "m/44'/60'/0'/0/", count: 10 };
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -44,7 +45,7 @@ const config: HardhatUserConfig = {
       chainId: 31337,
     },
     sepolia: {
-      accounts: [PRIVATE_KEY],
+      accounts: sepoliaAccounts,
       chainId: 11155111,
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
     },
